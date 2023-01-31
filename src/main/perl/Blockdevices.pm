60c4a447f4c575e1f235a1cfaefcf8e2a929c61e
James Adams <james@bluezen.co.uk>
2023-01-31 11:31:08 +0000

    Replace tabs in Blockdevices

--- src/main/perl/Blockdevices.pm
+++ src/main/perl/Blockdevices.pm
@@ -56,61 +56,61 @@ sub _initialize
 
 sub create
 {
-	my $self = shift;
-	$self->error ("create method not defined for this class");
+    my $self = shift;
+    $self->error ("create method not defined for this class");
 }
 
 sub remove
 {
-	my $self = shift;
-	$self->error ("remove method not defined for this class");
+    my $self = shift;
+    $self->error ("remove method not defined for this class");
 
 }
 
 sub grow
 {
-	my $self = shift;
-	$self->error ("grow method not defined for this class");
+    my $self = shift;
+    $self->error ("grow method not defined for this class");
 
 }
 
 sub shrink
 {
-	my $self = shift;
-	$self->error ("shrink method not defined for this class");
+    my $self = shift;
+    $self->error ("shrink method not defined for this class");
 
 }
 
 sub decide
 {
-	my $self = shift;
-	$self->error ("decide method not defined for this class");
+    my $self = shift;
+    $self->error ("decide method not defined for this class");
 }
 
 sub devexists
 {
-	my $self = shift;
-	$self->error ("devexists method not defined for this class");
+    my $self = shift;
+    $self->error ("devexists method not defined for this class");
 }
 
 
 sub should_print_ks
 {
-	my $self = shift;
-	$self->error ("should_print_ks method not defined for this class");
+    my $self = shift;
+    $self->error ("should_print_ks method not defined for this class");
 }
 
 sub should_create_ks
 {
-	my $self = shift;
-	$self->error ("should_create_ks method not defined for this class");
+    my $self = shift;
+    $self->error ("should_create_ks method not defined for this class");
 }
 
 # Return the size of metadata to wipe in MB
 sub get_clear_mb
 {
-	my $self = shift;
-	$self->error ("get_clear_mb is no longer implemented");
+    my $self = shift;
+    $self->error ("get_clear_mb is no longer implemented");
 }
 
 =pod
